eb34caa752f4ed780e2c97c85dff66c4a83b4845
Julian Zaiats <90129010+Combo3zoom@users.noreply.github.com>
2023-03-20 23:43:27 +0200

    style cop has been added

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,5 @@
+using llama_journal.Database;
 using Microsoft.EntityFrameworkCore;
-using Database;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,8 +16,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Error");
-	app.UseHsts();
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
 
 
