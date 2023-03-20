0e50bcd98a7cb22ccb7cdb02f683c70e591b4ad9
shevchenkoira <shevchenkoari@gmail.com>
2023-03-21 01:48:28 +0200

    add main files to built a structure of a project

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace llama_journal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using llama_journal.Data.Repositories;
+using llama_journal.Models;
+
+namespace llama_journal.Services
+{
+    public class UserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public List<User> GetUsers()
+        {
+            return _userRepository.GetUsers().ToList();
+        }
+    }
+}
